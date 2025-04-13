39372273270defc88dc14ccbefb84bafc4ef0b91
AbdallahSalah003 <abdallahsalahsalem9@gmail.com>
2025-04-13 08:46:49 +0200

    Implement Entity and World Classes

--- source/common/ecs/entity.hpp
+++ source/common/ecs/entity.hpp
@@ -35,7 +35,10 @@ namespace our {
             static_assert(std::is_base_of<Component, T>::value, "T must inherit from Component");
             //TODO: (Req 8) Create an component of type T, set its "owner" to be this entity, then push it into the component's list
             // Don't forget to return a pointer to the new component
-            return nullptr;
+            T *component = new T();
+            component->owner = this;
+            components.push_back(component);
+            return component;
         }
 
         // This template method searhes for a component of type T and returns a pointer to it
@@ -44,6 +47,12 @@ namespace our {
         T* getComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // Return the component you found, or return null of nothing was found.
+            for (auto *component : components)
+            {
+                T *cast_result = dynamic_cast<T *>(component);
+                if (cast_result)
+                    return cast_result;
+            }
             return nullptr;
         }
 
@@ -63,6 +72,16 @@ namespace our {
         void deleteComponent(){
             //TODO: (Req 8) Go through the components list and find the first component that can be dynamically cast to "T*".
             // If found, delete the found component and remove it from the components list
+            for (auto *component : components)
+            {
+                T *cast_result = dynamic_cast<T *>(component);
+                if (cast_result)
+                {
+                    delete component;
+                    components.remove(component);
+                    break;
+                }
+            }
         }
 
         // This template method searhes for a component of type T and deletes it
@@ -80,11 +99,15 @@ namespace our {
         void deleteComponent(T const* component){
             //TODO: (Req 8) Go through the components list and find the given component "component".
             // If found, delete the found component and remove it from the components list
+            components.remove(component);
+            delete component;
         }
 
         // Since the entity owns its components, they should be deleted alongside the entity
         ~Entity(){
             //TODO: (Req 8) Delete all the components in "components".
+            for (auto *component : components)
+                delete component;
         }
 
         // Entities should not be copyable
